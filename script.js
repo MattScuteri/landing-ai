3e382be669cf31207d289acb117b1630e8f109dd
MattScuteri <mttscuteri@gmail.com>
2019-10-20 15:36:48 -0400

    fixing sizing and shapes

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// var canvas = document.getElementById('cavnas');
-// var ctx = canvas.getContext('2d');
+var Engine = Matter.Engine
+var Render = Matter.Render
+var World = Matter.World
+var Bodies = Matter.Bodies;
 
-// canvas.width = innerWidth;
-// canvas.height = innerHeight;
+window.onload = function() {
+    var canvas = document.getElementById('canvas');
 
-
-function init() {
-    // module aliases
-    var Engine = Matter.Engine
-    var Render = Matter.Render
-    var World = Matter.World
-    var Bodies = Matter.Bodies;
-
+    canvas.height = innerHeight;
+    canvas.width = innerWidth;
+    
     // create an engine
     var engine = Engine.create();
 
     // create a renderer
     var render = Render.create({
         element: document.body,
-        engine: engine
+        canvas: canvas,
+        engine: engine,
+        options: {
+            width: canvas.width,
+            height: canvas.height
+        }
     });
 
-    // create two boxes and a ground
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    var ship = Bodies.polygon(400, 200, 3, 80, 80);
+    var ground = Bodies.rectangle(400, 610, canvas.width, 60, { isStatic: true });
 
     // add all of the bodies to the world
-    World.add(engine.world, [boxA, boxB, ground]);
+    World.add(engine.world, [ship, ground]);
 
     // run the engine
     Engine.run(engine);
@@ -35,5 +35,3 @@ function init() {
     // run the renderer
     Render.run(render);
 }
-
-init();
